017aaba9a3833571bc1753d33ec4bbf5931337d5
Orlando Santos <Santos.Orlando17@gmail.com>
2025-09-11 16:18:18 -1000

    add new text

--- src/components/PlugIn.tsx
+++ src/components/PlugIn.tsx
@@ -32,7 +32,7 @@ function PlugIn () {
       </div>
       <div>
         <div className="text-xs text-white text-center mt-4 mx-3 md:text-xl md:mx-8 lg:mt-5 lg:mx-16 lg:text-2xl lg:leading-relaxed">
-        One of the custom tools I developed integrates with a smart Detail Item Tag to address quality control issues. Previously, design changes often led to errors when manually updated areas were overlooked. The Update Areas tool I created populates a shared parameter with the area value from the Filled Region, which then automatically updates the tag. This workflow applies specifically to Filled Regions that contain Allowable Openings. By automating this process, the tool eliminated a tedious manual task and significantly improved quality assurance.
+        One of the custom tools I developed integrates with a smart Detail Item Tag to address quality control issues. Previously, design changes often led to errors when manually updated areas were overlooked. The Update Areas tool I created populates a shared parameter with the area value from the Filled Region, which then automatically updates the tag. This workflow applies specifically to Filled Regions that contain Allowable Openings. In addition, I created area tags for all opening types and a Tag Sum feature, which allows you to select multiple tags, calculates the total, and automatically copies the result for easy pasting. By automating these processes, the tool eliminated tedious manual tasks and significantly improved quality assurance.
         </div>
       </div>
       </div>
